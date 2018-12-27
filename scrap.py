0616c231dbcfee32a3a2298853614ffcfff52c0a
pixlmaster <akash.tiwari21aug@gmail.com>
2018-12-27 14:13:56 +0530

    bokr into functions

--- scrap.py
+++ scrap.py
@@ -15,6 +15,7 @@ import requests
 kissanime_login_url="https://kissanime.ru/Login"
 kissanime_base_url="https://kissanime.ru"
 kissanime_main_url=kissanime_base_url+"/"
+kissanime_search_url=kissanime_main_url+"Search/Anime"
 user_name="testkgp"
 user_password="qwertyuiop"
 
@@ -47,99 +48,128 @@ def site_login(browser):
 
 def site_search(browser):
 	browser.get(kissanime_main_url)
-
-	WebDriverWait(browser, 30)
-
 	main_window=browser.current_window_handle
 
 	WebDriverWait(browser,30).until(EC.presence_of_element_located((By.ID,"keyword")))
-	WebDriverWait(browser,30).until(EC.visibility_of_element_located((By.ID,"keyword")))
+	#WebDriverWait(browser,30).until(EC.visibility_of_element_located((By.ID,"keyword")))
+	
 	inputelement=browser.find_element_by_id("keyword")
-
 	inputelement.send_keys("boruto")
 
 	searchButton = browser.find_element_by_id("imgSearch")
 	searchButton.click()
-
-	while browser.current_url != "https://kissanime.ru/Search/Anime":
-	     ActionChains(browser).send_keys(Keys.CONTROL + 'w').perform()
+	list = browser.window_handles
+	for ad in list:
+		browser.switch_to_window(ad)
+		if ad!=main_window:
+			browser.close()
 
 	browser.switch_to_window(main_window)
 
+	return browser,main_window
+
+def search_result(browser):
+	soup= bs(browser.page_source, 'html.parser')
+
+	table=soup.find('table')
+
+	links = table.find_all("a")
+	i=1
+	for link in links:
+		anime=kissanime_base_url+link.get('href')
+		title=(link.get_text()).strip()
+		print(str(i) +". "+title)
+		i+=1
+	print("Enter which you want to download?")
+	x= input()
+
+	url=kissanime_base_url+links[int(x)-1].get('href')
+
+	browser.get(url)
+	WebDriverWait(browser, 5)
+
+	browser.switch_to_window(main_window)
 	return browser
 
+def episode_result(browser):
+	anime_soup= bs(browser.page_source, 'html.parser')
+	anime_table=anime_soup.find('table')
+
+	episodes = anime_table.find_all("a")
+	i=1
+	for episode in episodes:
+		print(str(i)+"."+(episode.get_text()).strip())
+		i+=1
+	print("enter serial no to download")
+	x= input()
+	x=int(x)
+
+	anime_url=kissanime_base_url + episodes[x-1].get('href')
+
+	browser.get(anime_url)
+	WebDriverWait(browser, 5)
+	browser.switch_to_window(main_window)
+	return browser
+
+def skip(browser):
+	skip_button=browser.find_element_by_class_name('specialButton')
+
+	skip_button.click()
+	WebDriverWait(browser, 5)
+
+	return browser
+
+def anime_page(browser):
+	download_soup=bs(browser.page_source, 'html.parser')
+
+	download_l=download_soup.find("div",{"id":"divDownload"})
+
+	download_link=download_l.find('a')
+	download_final=download_link['href']
+
+	return browser, download_final
+
+def download_page(browser):
+	browser.get(download_final)
+
+	final_soup=bs(browser.page_source, 'html.parser')
+
+	resolutions=final_soup.find_all(id="button-download")
+	i=1
+	for resolution in resolutions:
+		print(str(i)+"."+(resolution.get_text()).strip())
+		i+=1
+
+	print("enter resolution to download")
+	x=input()
+	x=int(x)
+
+	anime_link=resolutions[x-1].get('href')
+	browser.get(anime_link)
 
 driver=browser_init()
 
-site_login(driver)
+try:
+	site_login(driver)
 
-driver=site_search(driver)
+	driver,main_window=site_search(driver)
+
+	driver=search_result(driver)
+
+	driver=episode_result(driver)
+
+	driver=skip(driver)
+
+	driver,download_final=anime_page(driver)
+
+	driver=download_page(driver)
+
+except Exception as e:
+	print("an error occurred")
+	print(e)
+finally:
+	driver.quit()
 
 
-soup= bs(driver.page_source, 'html.parser')
 
-table=soup.find('table')
 
-links = table.find_all("a")
-i=1
-for link in links:
-	anime="https://kissanime.ru"+link.get('href')
-	title=(link.get_text()).strip()
-	print(str(i) +". "+title)
-	i+=1
-print("Enter which you want to download?")
-x= input()
-
-url="https://kissanime.ru"+links[int(x)-1].get('href')
-
-driver.get(url)
-
-WebDriverWait(driver, 10)
-driver.switch_to_window(main_window)
-
-anime_soup= bs(driver.page_source, 'html.parser')
-anime_table=anime_soup.find('table')
-
-episodes = anime_table.find_all("a")
-i=1
-for episode in episodes:
-	print(str(i)+"."+(episode.get_text()).strip())
-	i+=1
-print("enter serial no to download")
-x= input()
-x=int(x)
-
-anime_url="https://kissanime.ru" + episodes[x-1].get('href')
-
-driver.get(anime_url)
-WebDriverWait(driver, 10)
-driver.switch_to_window(main_window)
-
-skip_button=driver.find_element_by_class_name('specialButton')
-
-skip_button.click()
-WebDriverWait(driver, 10)
-
-download_soup=bs(driver.page_source, 'html.parser')
-
-download_l=download_soup.find("div",{"id":"divDownload"})
-
-download_link=download_l.find('a')
-download_final=download_link['href']
-
-driver.get(download_final)
-
-final_soup=bs(driver.page_source, 'html.parser')
-
-resolutions=final_soup.find_all(id="button-download")
-i=1
-for resolution in resolutions:
-	print(str(i)+"."+(resolution.get_text()).strip())
-	i+=1
-
-print("enter resolution to download")
-x=input()
-x=int(x)
-
-anime_link=resolutions[x-1].get('href')
-driver.get(anime_link)
